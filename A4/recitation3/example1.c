7a5ac61e56cfe82ff62a5e52715f44e1b853d17d
SunsetHe <2622319129@qq.com>
2023-11-28 00:34:54 +0000

    整理A4

--- A4/recitation3/example1.c
+++ A4/recitation3/example1.c
@@ -2,14 +2,23 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-#include <math.h>
+#include <immintrin.h>  // 包含 AVX2 头文件
 
 #define SIZE (1L << 16)
 
-void test(uint8_t * a,  uint8_t * b) {
+void test(uint8_t * restrict a, uint8_t * restrict b) {
   uint64_t i;
 
-  for (i = 0; i < SIZE; i++) {
-    a[i] += b[i];
+  // 使用 _mm256_load_si256 函数进行对齐加载
+  __m256i* avx_a = (__m256i*)a;
+  __m256i* avx_b = (__m256i*)b;
+
+  for (i = 0; i < SIZE / 32; i++) {
+    // 使用 AVX2 指令进行矢量化的字节相加
+    __m256i avx_result = _mm256_add_epi8(_mm256_load_si256(avx_a + i), _mm256_load_si256(avx_b + i));
+    
+    // 使用 _mm256_store_si256 函数进行对齐存储
+    _mm256_store_si256(avx_a + i, avx_result);
   }
 }
+
